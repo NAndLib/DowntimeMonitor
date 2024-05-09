0f6dd471600b05e818063b82bf13d3a617fb79e2
Andrew Tran <andremail03@gmail.com>
2024-05-08 20:08:32 -0700

    chore: run yapf

--- monitorNetwork.py
+++ monitorNetwork.py
@@ -14,13 +14,16 @@ DEFAULT_PORT = 443
 DEFAULT_TIMEOUT = 5
 LOG_FMT = "{timestamp}\t{host}:{port}\t{status}\n"
 
-def internet( host: str = DEFAULT_HOST, port: int = DEFAULT_PORT,
-              timeout: int = DEFAULT_TIMEOUT, verbose: bool = False ) -> bool:
+
+def internet(host: str = DEFAULT_HOST,
+             port: int = DEFAULT_PORT,
+             timeout: int = DEFAULT_TIMEOUT,
+             verbose: bool = False) -> bool:
     """
     Generates a small HEAD request to verify connectivity
     """
-    if host.startswith( 'http' ):
-        host = urlparse( host ).netloc
+    if host.startswith('http'):
+        host = urlparse(host).netloc
     conn = httplib.HTTPSConnection(host, port, timeout=timeout)
     try:
         if verbose:
@@ -46,80 +49,116 @@ def internet( host: str = DEFAULT_HOST, port: int = DEFAULT_PORT,
     finally:
         conn.close()
 
+
 def timestamp():
     """ Get Current timestamp string """
     return datetime.datetime.now().isoformat()
 
+
 def parseArgs():
-    parser = argparse.ArgumentParser( description="Monitor network connection" )
+    parser = argparse.ArgumentParser(description="Monitor network connection")
 
     parser.add_argument(
-        '--host', '-H', dest='host', default=DEFAULT_HOST,
-        help=f"Monitor using the given host. Default: {DEFAULT_HOST}" )
-    parser.add_argument( '--port', '-p', default=DEFAULT_PORT, help=f"Use the given port. Default: {DEFAULT_PORT}" )
+        '--host',
+        '-H',
+        dest='host',
+        default=DEFAULT_HOST,
+        help=f"Monitor using the given host. Default: {DEFAULT_HOST}")
+    parser.add_argument('--port',
+                        '-p',
+                        default=DEFAULT_PORT,
+                        help=f"Use the given port. Default: {DEFAULT_PORT}")
     parser.add_argument(
-        '--overwrite-log', '-O', dest='overwrite', action='store_true',
-        help="Overwrite the log file if it exists instead of appending" )
-    parser.add_argument( '--logFile', '-l', default="connection.log",
-                         help="Name of log file to use. Default: connection log" )
-    parser.add_argument( '--stdout', action='store_true',
-                         help="Use stdout instead of output file" )
-    parser.add_argument( '--interval', '-i', metavar='SECONDS', type=int, default=5,
-                         help="Time in seconds to wait before checking "
-                              "the host again. Default: 5." )
-    parser.add_argument( '--timeout', '-t', metavar='SECONDS', type=int, default=1,
-                         help="Timeout in seconds for connection check. Default: 1")
-    parser.add_argument( '--verbose', '-v', action='store_true',
-                         help="Verbose output")
-    parser.add_argument( '--daemon', '-d', action='store_true',
-                         help="Tells the script to run in daemon mode")
+        '--overwrite-log',
+        '-O',
+        dest='overwrite',
+        action='store_true',
+        help="Overwrite the log file if it exists instead of appending")
+    parser.add_argument(
+        '--logFile',
+        '-l',
+        default="connection.log",
+        help="Name of log file to use. Default: connection log")
+    parser.add_argument('--stdout',
+                        action='store_true',
+                        help="Use stdout instead of output file")
+    parser.add_argument('--interval',
+                        '-i',
+                        metavar='SECONDS',
+                        type=int,
+                        default=5,
+                        help="Time in seconds to wait before checking "
+                        "the host again. Default: 5.")
+    parser.add_argument(
+        '--timeout',
+        '-t',
+        metavar='SECONDS',
+        type=int,
+        default=1,
+        help="Timeout in seconds for connection check. Default: 1")
+    parser.add_argument('--verbose',
+                        '-v',
+                        action='store_true',
+                        help="Verbose output")
+    parser.add_argument('--daemon',
+                        '-d',
+                        action='store_true',
+                        help="Tells the script to run in daemon mode")
 
     return parser.parse_args()
 
-def writeLog( file: TextIO, host: str, port: int, status: str ):
-    file.write( LOG_FMT.format( timestamp=timestamp(),
-                                host=host,
-                                port=port,
-                                status=status ) )
+
+def writeLog(file: TextIO, host: str, port: int, status: str):
+    file.write(
+        LOG_FMT.format(timestamp=timestamp(),
+                       host=host,
+                       port=port,
+                       status=status))
     file.flush()
+
 
 if __name__ == '__main__':
     args = parseArgs()
     if args.daemon:
-        sys.stderr.write( "Deamon mode currently unsupported" )
+        sys.stderr.write("Deamon mode currently unsupported")
         exit(1)
 
     logFile = args.logFile
     outFile = None
     if args.stdout:
         outFile = sys.stdout
-    elif not os.path.isfile( logFile ) or args.overwrite:
-        outFile = open( logFile, 'w', encoding="UTF-8" )
+    elif not os.path.isfile(logFile) or args.overwrite:
+        outFile = open(logFile, 'w', encoding="UTF-8")
     else:
-        outFile = open( logFile, 'a', encoding="UTF-8" )
+        outFile = open(logFile, 'a', encoding="UTF-8")
 
     host = args.host
     port = args.port
-    print( f"Starting monitoring with {host} at {timestamp()}" )
+    print(f"Starting monitoring with {host} at {timestamp()}")
     startTime = datetime.datetime.now()
     try:
         while True:
-            ok = internet( host, port, timeout=args.timeout, verbose=args.verbose )
+            ok = internet(host,
+                          port,
+                          timeout=args.timeout,
+                          verbose=args.verbose)
             if ok:
-                writeLog( outFile, host, port, 'OK' )
+                writeLog(outFile, host, port, 'OK')
             else:
-                writeLog( outFile, host, port, 'NOT CONNECTED' )
+                writeLog(outFile, host, port, 'NOT CONNECTED')
 
             elapsed = datetime.datetime.now() - startTime
             if args.stdout or args.daemon:
-                sys.stdout.write( f"Elapsed time: {elapsed} "
-                                  f"[{'OK' if ok else 'NOT CONNECTED'}]{' ' * 20}\n" )
+                sys.stdout.write(
+                    f"Elapsed time: {elapsed} "
+                    f"[{'OK' if ok else 'NOT CONNECTED'}]{' ' * 20}\n")
             else:
-                sys.stdout.write( f"Elapsed time: {elapsed} "
-                                  f"[{'OK' if ok else 'NOT CONNECTED'}]{' ' * 20}\r" )
+                sys.stdout.write(
+                    f"Elapsed time: {elapsed} "
+                    f"[{'OK' if ok else 'NOT CONNECTED'}]{' ' * 20}\r")
             sys.stdout.flush()
 
-            sleep( args.interval )
+            sleep(args.interval)
     except KeyboardInterrupt:
-        print( f"\nStopping monitor at {timestamp()}" )
+        print(f"\nStopping monitor at {timestamp()}")
     outFile.close()
