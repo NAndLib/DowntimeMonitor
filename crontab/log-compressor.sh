40495163e00a8c6333b011e256b0a13b75805ae8
Andrew Tran <andremail03@gmail.com>
2024-05-08 20:34:24 -0700

    feat!: log-compressor.sh now deletes the compressed files

--- crontab/log-compressor.sh
+++ crontab/log-compressor.sh
@@ -14,5 +14,6 @@ if ! [[ -d "$LOG_DIR" ]]; then
   echo "No directory $LOG_DIR found."
 fi
 
-DATE=$(date -I)
+DATE=$(date -Ins)
 find "$LOG_DIR" -type f -size +100M -exec lrzip -g -o "$LOG_DIR/$DATE.lrz" {} \;
+find "$LOG_DIR" -type f -size +100M -name "*.log" -exec rm -f {} \;
